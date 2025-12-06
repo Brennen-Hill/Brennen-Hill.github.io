7c89f501ed7971591b2c782d3a76124358a80fa5
Brennen-Hill <bahill4@wisc.edu>
2025-12-06 16:09:03 +0000

    Deploying to gh-pages from @ Brennen-Hill/Brennen-Hill.github.io@ddc9cf526ec2ce8366efad780548eb6089b8527a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,14 +37,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "presentations-",
-          title: '',
-          description: "",
+        },{id: "presentations-co-evolving-complexity-an-adversarial-framework-for-automatic-marl-curricula",
+          title: 'Co-Evolving Complexity: An Adversarial Framework for Automatic MARL Curricula',
+          description: "Presented at the NeuriPS Workshop on Scaling Environments for Agents",
           section: "Presentations",handler: () => {
               window.location.href = "/presentations/Co-Evolving-Complexity/";
-            },},{id: "presentations-",
-          title: '',
-          description: "",
+            },},{id: "presentations-communicating-plans-not-percepts-scalable-multi-agent-coordination-with-embodied-world-models",
+          title: 'Communicating Plans, Not Percepts Scalable Multi-Agent Coordination with Embodied World Models',
+          description: "Presented at NeuriPS Workshop",
           section: "Presentations",handler: () => {
               window.location.href = "/presentations/Communicating-Plans-Not-Percepts/";
             },},{id: "presentations-title",
