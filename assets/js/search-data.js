dbaace5a333c82c7c5cdc27604c5a632af0e50cd
Brennen-Hill <bahill4@wisc.edu>
2025-12-06 15:28:00 +0000

    Deploying to gh-pages from @ Brennen-Hill/Brennen-Hill.github.io@21a6728baaf9e9982f3d2c18962b0bed54e6b088 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-presentations",
-          title: "presentations",
-          description: "A collection of my posters, talks, and conference presentations.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/presentations/";
-          },
         },{id: "nav-projects",
           title: "projects",
           description: "",
