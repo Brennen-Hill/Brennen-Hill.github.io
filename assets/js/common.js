fc1303f04e04f6a40a1f946eac048b4d6e1b0478
Brennen Hill <bhill@aurelianindustries.com>
2025-12-09 14:56:55 -0600

    f

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,20 +1,24 @@
-$(document).ready(function () {
-  // add toggle functionality to abstract, award and bibtex buttons
-  $("a.abstract").click(function () {
-    $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
-  });
-  $("a.award").click(function () {
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open");
-  });
-  $("a.bibtex").click(function () {
-    $(this).parent().parent().find(".abstract.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".award.hidden.open").toggleClass("open");
-    $(this).parent().parent().find(".bibtex.hidden").toggleClass("open");
-  });
+$(document).ready(function() {
+  // Helper function to toggle a specific section and close others
+  var toggleSection = function(button, type) {
+      var parent = button.parent().parent();
+      
+      // Toggle the clicked content block
+      parent.find('.' + type + '.hidden').toggleClass('open');
+      
+      // Close any other open blocks (abstract, bibtex, etc.) in this entry
+      parent.find('.hidden.open').not('.' + type).removeClass('open');
+  };
+
+  // Add click listeners to all buttons
+  $("a.abstract").click(function() { toggleSection($(this), 'abstract'); });
+  $("a.bibtex").click(function() { toggleSection($(this), 'bibtex'); });
+  $("a.award").click(function() { toggleSection($(this), 'award'); });
+  $("a.pdf").click(function() { toggleSection($(this), 'pdf'); });
+  $("a.video").click(function() { toggleSection($(this), 'video'); });
+  $("a.poster").click(function() { toggleSection($(this), 'poster'); });
+
+
   $("a").removeClass("waves-effect waves-light");
 
   // bootstrap-toc
